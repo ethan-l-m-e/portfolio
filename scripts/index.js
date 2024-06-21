cbdb7a9758b7f1168e8335fc5f895cf3600add7e
Lim Ethan <69660935+ethan-l-m-e@users.noreply.github.com>
2024-06-21 10:04:41 +0800

    Add chess to list of projects
    Also reformatted document.

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
+  document.addEventListener('DOMContentLoaded', ready);
 } else {
-    ready();
+  ready();
 }
 
 function forEach(array, action) {
-    for (var i = 0; i < array.length; i++) {
-        action(array[i]);
-    }
+  for (var i = 0; i < array.length; i++) {
+    action(array[i]);
+  }
 }
 
 function createProjectCard(name, githubLink, playLink, imageSrc) {
-    const template = 
-        `<div class="project-card shadow-effect">
+  const template =
+    `<div class="project-card shadow-effect">
             <div class="card-header micro-5-regular">
                 <div class="title-text">${name}</div>
                 <div class="external-links">
@@ -26,63 +26,69 @@ function createProjectCard(name, githubLink, playLink, imageSrc) {
                 <img class="thumbnail" src="${imageSrc}" alt="Thumbnail of a game of ${name}.">
             </div>
         </div>`;
-    return template;
+  return template;
 }
 
 function insertProjectsIntoHTML(arrayOfProjects) {
-    const projectListElement = document.getElementsByClassName("project-list")[0];
+  const projectListElement = document.getElementsByClassName("project-list")[0];
 
-    var elements = [];
-    forEach(arrayOfProjects, function(project) {
-        var card = createProjectCard(
-            project.name, 
-            project.githubLink, 
-            project.playLink, 
-            project.imageSrc);
-        elements.push(card);
-    });
-    projectListElement.insertAdjacentHTML('beforeend', elements.join(""));
+  var elements = [];
+  forEach(arrayOfProjects, function (project) {
+    var card = createProjectCard(
+      project.name,
+      project.githubLink,
+      project.playLink,
+      project.imageSrc);
+    elements.push(card);
+  });
+  projectListElement.insertAdjacentHTML('beforeend', elements.join(""));
 }
 
 const listOfProjects = [
-    {
-        name: "Flappy Bird",
-        githubLink: "https://github.com/ethan-l-m-e/flappy_bird_clone",
-        playLink: "https://ethan-l-m-e.github.io/flappy_bird_clone/",
-        imageSrc: "./images/flappy-bird.jpg"
-    },
-    {
-        name: "Sudoku",
-        githubLink: "https://github.com/ethan-l-m-e/sudoku_game",
-        playLink: "https://ethan-l-m-e.github.io/sudoku_game/",
-        imageSrc: "./images/sudoku.jpg"
-    },
-    {
-        name: "Tic-tac-toe",
-        githubLink: "https://github.com/ethan-l-m-e/tic_tac_toe",
-        playLink: "https://ethan-l-m-e.github.io/tic_tac_toe/",
-        imageSrc: "./images/tic-tac-toe.jpg"
-    },
-    {
-        name: "Pong",
-        githubLink: "https://github.com/ethan-l-m-e/pong",
-        playLink: "https://ethan-l-m-e.github.io/pong/",
-        imageSrc: "./images/pong.jpg"
-    },
-    {
-        name: "Guess The Number",
-        githubLink: "https://github.com/ethan-l-m-e/guess_the_number_game",
-        playLink: "https://ethan-l-m-e.github.io/guess_the_number_game/",
-        imageSrc: "./images/guess-the-number.jpg"
-    },
-    {
-        name: "Dodgeball",
-        githubLink: "https://github.com/ethan-l-m-e/dodgeball",
-        playLink: "", // No link.
-        imageSrc: "./images/dodgeball.jpg"
-    }
+  {
+    name: "Chess",
+    githubLink: "https://github.com/ethan-l-m-e/chess",
+    playLink: "https://ethan-l-m-e.github.io/chess/",
+    imageSrc: "./images/chess.jpg",
+  },
+  {
+    name: "Flappy Bird",
+    githubLink: "https://github.com/ethan-l-m-e/flappy_bird_clone",
+    playLink: "https://ethan-l-m-e.github.io/flappy_bird_clone/",
+    imageSrc: "./images/flappy-bird.jpg",
+  },
+  {
+    name: "Sudoku",
+    githubLink: "https://github.com/ethan-l-m-e/sudoku_game",
+    playLink: "https://ethan-l-m-e.github.io/sudoku_game/",
+    imageSrc: "./images/sudoku.jpg",
+  },
+  {
+    name: "Tic-tac-toe",
+    githubLink: "https://github.com/ethan-l-m-e/tic_tac_toe",
+    playLink: "https://ethan-l-m-e.github.io/tic_tac_toe/",
+    imageSrc: "./images/tic-tac-toe.jpg",
+  },
+  {
+    name: "Pong",
+    githubLink: "https://github.com/ethan-l-m-e/pong",
+    playLink: "https://ethan-l-m-e.github.io/pong/",
+    imageSrc: "./images/pong.jpg",
+  },
+  {
+    name: "Guess The Number",
+    githubLink: "https://github.com/ethan-l-m-e/guess_the_number_game",
+    playLink: "https://ethan-l-m-e.github.io/guess_the_number_game/",
+    imageSrc: "./images/guess-the-number.jpg",
+  },
+  {
+    name: "Dodgeball",
+    githubLink: "https://github.com/ethan-l-m-e/dodgeball",
+    playLink: "", // No link.
+    imageSrc: "./images/dodgeball.jpg",
+  },
 ]
 
 function ready() {
-    insertProjectsIntoHTML(listOfProjects);
+  insertProjectsIntoHTML(listOfProjects);
 }
